a665e4d3a4a7cc1698f69a1e7ab0108ecaf5e023
Gail Parazo <202310763@gordoncollege.edu.ph>
2024-12-01 11:34:30 +0800

    Strlen Function

--- FinalsSrc.java
+++ FinalsSrc.java
@@ -2,9 +2,10 @@ import javax.swing.*;
 
 public class FinalsSrc {
     private JTextArea screen;
-
+    
     public FinalsSrc(JTextArea screen) {
         this.screen= screen;
+        
     }
     public void SetScreenEditableBtn(){
     screen.setEditable(true);
@@ -13,4 +14,15 @@ public class FinalsSrc {
     int stringLenght = screen.getText().length();
     screen.setText("≽^•⩊•^≼ string lenght: " +stringLenght);
  }
+ public void ConcatenateStringBtn () {
+   
+   String inputText = screen.getText(); // Get the current text from the JTextArea
+   
+      
+       
+   }
 }
+
+ 
+   
+
